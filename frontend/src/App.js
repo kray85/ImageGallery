4851a98e5f07bdd47c81437a9bc27fb2b4a58ba2
Kendal Brown <kendal.r.brown.ctr@us.navy.mil>
2024-10-28 20:00:55 -0400

    Initital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, React 18!</h1>
+      <p>Check the console for the logs.</p>
+    </div>
+  )
+}
+
+export default App
